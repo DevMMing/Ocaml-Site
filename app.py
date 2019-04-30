3aa0b597384a5a2b5eb05a5f88bd2b4ee2fd1b25
DevMMing <mming@stuy.edu>
2019-04-30 07:24:56 -0400

    hm

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 #Ocaml Presentation -- Matthew Ming
-
-import os, csv
 from flask import Flask, render_template, redirect, url_for, session, request, flash, get_flashed_messages
 
 app = Flask(__name__)
-app.secret_key = os.urandom(32)
-
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
